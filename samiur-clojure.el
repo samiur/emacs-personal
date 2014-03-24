a3abd9fe204085c7c3b7e02bc05e0b5b6ec8abfa
Samiur Rahman <samiur@jobsuitors.com>
2014-03-24 08:39:42 -0700

    Clojure mode changes
    - Add AC-nrepl, rainbow delimiters, clj-refactor, clojure-snippets and
      kibit
    - Add keybord shortcuts

--- samiur-clojure.el
+++ samiur-clojure.el
@@ -1,3 +1,5 @@
+(prelude-require-packages '(ac-nrepl rainbow-delimiters clj-refactor clojure-snippets kibit-mode))
+
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 
 (require 'ac-nrepl)
@@ -10,12 +12,17 @@
   (setq completion-at-point-functions '(auto-complete)))
 (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-(add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-repl-mode-hook
+          (lambda ()
+            (cider-turn-on-eldoc-mode)))
 
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook
+          (lambda ()
+            (cider-turn-on-eldoc-mode)))
+
+(setq cider-popup-stacktraces nil)
+(setq cider-popup-stacktraces-in-repl nil)
+(add-to-list 'same-window-buffer-names "*cider*")
 
 (add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
 (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
@@ -23,5 +30,74 @@
 (eval-after-load "cider"
   '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
 
+(defun live-nrepl-set-print-length ()
+  (nrepl-send-string-sync "(set! *print-length* 100)" "clojure.core"))
+
+(add-hook 'nrepl-connected-hook 'live-nrepl-set-print-length)
+
 (require 'rainbow-delimiters)
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "λ")
+                               nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("\\(#\\)("
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "ƒ")
+                               nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("\\(#\\){"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "∈")
+                               nil))))))
+
+(eval-after-load 'find-file-in-project
+  '(add-to-list 'ffip-patterns "*.clj"))
+
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (setq buffer-save-without-query t)))
+
+;;Treat hyphens as a word character when transposing words
+(defvar clojure-mode-with-hyphens-as-word-sep-syntax-table
+  (let ((st (make-syntax-table clojure-mode-syntax-table)))
+    (modify-syntax-entry ?- "w" st)
+    st))
+
+(defun live-transpose-words-with-hyphens (arg)
+  "Treat hyphens as a word character when transposing words"
+  (interactive "*p")
+  (with-syntax-table clojure-mode-with-hyphens-as-word-sep-syntax-table
+    (transpose-words arg)))
+
+(define-key clojure-mode-map (kbd "M-t") 'live-transpose-words-with-hyphens)
+
+(setq auto-mode-alist (append '(("\\.cljs$" . clojure-mode))
+                              auto-mode-alist))
+
+;; Pull in the awesome clj-refactor lib by magnars
+(require 'clj-refactor)
+(add-hook 'clojure-mode-hook (lambda ()
+                               (clj-refactor-mode 1)
+                               (cljr-add-keybindings-with-prefix "C-c C-m")))
+
+(define-key clojure-mode-map (kbd "C-:") 'cljr-cycle-stringlike)
+(define-key clojure-mode-map (kbd "C->") 'cljr-cycle-coll)
+
+(defun live-warn-when-cider-not-connected ()
+      (interactive)
+      (message "nREPL server not connected. Run M-x cider or M-x cider-jack-in to connect."))
+
+(define-key clojure-mode-map (kbd "C-M-x")   'live-warn-when-cider-not-connected)
+(define-key clojure-mode-map (kbd "C-x C-e") 'live-warn-when-cider-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-e") 'live-warn-when-cider-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-l") 'live-warn-when-cider-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-r") 'live-warn-when-cider-not-connected)
